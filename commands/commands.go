d490350b448031c3714095942c4fc639fcc5e70b
jcmdln <jcmdln@gmail.com>
2019-02-15 20:13:40 -0500

    Add Commands.List() []string

--- commands/commands.go
+++ commands/commands.go
@@ -30,6 +30,14 @@ func (c *Commands) Add(cmd string, cmdr Commander) {
 	c.cmds[cmd] = cmdr
 }
 
+func (c *Commands) List() []string {
+	var cmds []string
+	for cmd := range c.cmds {
+		cmds = append(cmds, cmd)
+	}
+	return cmds
+}
+
 func (c Commands) Parse(flags []string) error {
 	if len(flags) > 0 {
 		if v, ok := c.cmds[flags[0]]; ok {
